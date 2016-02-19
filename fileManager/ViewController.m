b8e5964e243d0d09c8b0899a834801bd34680143
2586675753 <2586675753@qq.com>
2016-02-19 15:40:49 +0800

    viewdidload

--- fileManager/ViewController.m
+++ fileManager/ViewController.m
@@ -17,7 +17,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    NSArray  *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docPath = [paths lastObject];
+    
+    NSString *docPathStr = [docPath stringByAppendingPathComponent:@"24.jpg"];
+    
+    NSData *data = [fileManager contentsAtPath:docPathStr];
+    
+    UIImage *image = [UIImage imageWithData:data];
+    
+//    BOOL tf = [fileManager fileExistsAtPath:docPathStr];
+//    
+//    if (tf) {
+//        [fileManager removeItemAtPath:docPathStr error:nil];
+//    }
+    
+    long long size = [self fileSizeAtPath:docPathStr];
+    long long sss = [self folderSizeAtPath:docPath];
+    
+   
+    
+    
 }
+
+- (long long) fileSizeAtPath:(NSString*) filePath{
+    
+    NSFileManager* manager = [NSFileManager defaultManager];
+    
+    if ([manager fileExistsAtPath:filePath]){
+        
+        return [[manager attributesOfItemAtPath:filePath error:nil] fileSize];
+    }
+    return 0;
+}
+
+
+//遍历文件夹获得文件夹大小，返回多少M
+
+- (float) folderSizeAtPath:(NSString*) folderPath{
+    
+    NSFileManager* manager = [NSFileManager defaultManager];
+    
+    if (![manager fileExistsAtPath:folderPath]) return 0;
+    
+    NSEnumerator *childFilesEnumerator = [[manager subpathsAtPath:folderPath] objectEnumerator];
+    
+    NSString* fileName;
+    
+    long long folderSize = 0;
+    
+    while ((fileName = [childFilesEnumerator nextObject]) != nil){
+        
+        NSString* fileAbsolutePath = [folderPath stringByAppendingPathComponent:fileName];
+        
+        folderSize += [self fileSizeAtPath:fileAbsolutePath];
+        
+    }
+    
+    return folderSize/(1024.0);
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
